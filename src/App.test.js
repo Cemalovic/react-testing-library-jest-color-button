79a2d7781da198b995dde69317db069812dd5dad
Milos Cemalovic <Milos.Cemalovic@rs.ey.com>
2022-04-20 13:43:34 +0200

    Lesson 1-17

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />)
+//   // const linkElement = screen.getByText(/learn react/i)
+//   // const linkElement = screen.getByText('Learn React')
+//   const linkElement = screen.getByRole('link', { name: /learn react/i })
+//   expect(linkElement).toBeInTheDocument()
+// })
+
+test('button has correct initial collor', () => {
+  render(<App />)
+
+  // find an element with a role of 'button' and a text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+
+  // expect the background color to be red // mora da bude inline-style
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+  // click button
+  fireEvent.click(colorButton)
+
+  // expect the background color to be blue // mora da bude inline-style
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red')
+})
+
+test('initial conditions', () => {
+  render(<App />)
+
+  // check that the button starts 'enabled'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+  expect(colorButton).toBeEnabled()
+
+  // check that the checkbox starts 'unchecked'
+  const checkbox = screen.getByRole('checkbox')
+  expect(checkbox).not.toBeChecked()
+})
